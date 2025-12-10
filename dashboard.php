dd4f2757555fdcbe98704dccab29775355ca2d0a
Max Bisschop <max@ffkijken.com>
2025-12-10 23:16:18 +0100

    favicon

--- dashboard.php
+++ dashboard.php
@@ -59,6 +59,7 @@ $classes = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Picker - Dashboard</title>
+    <link rel="icon" type="image/png" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
